94ca14ca7379d53c2b4ec1c0ace7493f06f76af5
Neil Logan <Logan8@gmail.com>
2017-06-11 03:30:50 +0100

    KowledgeItem framework.

---
+++ NaturalPowerWindTurbines/Controllers/KnowledgeItemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NaturalPowerWindTurbines.Models;
+
+namespace NaturalPowerWindTurbines.Controllers
+{
+    public class KnowledgeItemController : Controller
+    {
+        // GET: KnowledgeItem
+        public ActionResult Index()
+        {
+            var knowledgeItem = new KnowledgeItem() { Details = "A Wind Turbine" };
+
+            return View(knowledgeItem);
+        }
+    }
+}
---
+++ NaturalPowerWindTurbines/Models/KnowledgeItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NaturalPowerWindTurbines.Models
+{
+    public class KnowledgeItem
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Details { get; set; }
+
+        public string WindFarm { get; set; }
+
+        public DateTime DateOfInformation { get; set; }
+
+        /*[Required]
+        public Manufacturer ManufacturerID { get; set; }
+
+        [Required]
+        public EntryStatus EntryStatusID { get; set; }
+
+        [Required]
+        public KnowledgeCategory KnowlwdgeCateogory { get; set; }*/
+
+        [Required]
+        public string CreatedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+    }
+}
